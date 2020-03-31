8d425b146ef97d14036f916eaa8ca75ed38b6953
siti alawiah <alawiahsiti13@gmail.com>
2020-03-31 19:16:10 +0700

    Tugas1

---
+++ src/kelasutama.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sitialawiah
+ */
+public class kelasutama {
+    public static void main(String[] args){
+        futama futama = new futama();
+        futama.setVisible(true);
+    }
+}
